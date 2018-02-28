b2050c628b23613830e72b0eedb40c1bc20a201c
Jonathan <jonathan.frere@gmail.com>
2018-02-28 11:49:50 +0000

    Add login button and screen

--- src/js/components/introduction/Introduction.tsx
+++ src/js/components/introduction/Introduction.tsx
@@ -6,18 +6,48 @@ import { Button } from '../stylish/buttons';
 import styles from './Introduction.scss';
 
 type IntroductionProps = { languagesLoaded: boolean } & Callback<'onBeginQuestions', void>;
+type IntroductionState = { showLogin: boolean };
 
-export function Introduction({ languagesLoaded, onBeginQuestions }: IntroductionProps) {
-    return <div className={styles.introScreen}>
-        <header className={styles.header}>
-            <h1>Modern Day Slavery&nbsp;App</h1>
-        </header>
-        <nav className={styles.menu}>
-            {languagesLoaded
-                ? <Button className={styles.button}
-                          onClick={onBeginQuestions}>Begin Questions</Button>
-                : <Button className={styles.button} disabled={true}>Loading Data...</Button>}
-            <Button className={styles.button}>Help</Button>
-        </nav>
+function LoginScreen() {
+    return <div className={styles.loginScreen}>
+        <label htmlFor="email">Email:</label><input id="email" type="text"/>
+        <label htmlFor="password">Password:</label><input id="password" type="password"/>
+        <Button>Login</Button>
     </div>;
+}
+
+export class Introduction extends React.Component<IntroductionProps, IntroductionState> {
+
+    state = {
+        showLogin: false,
+    };
+
+    constructor(props: IntroductionProps) {
+        super(props);
+    }
+
+    showLoginScreen() {
+        this.setState({ showLogin: true });
+    }
+
+    render() {
+        return <div className={styles.introScreen}>
+            <header className={styles.header}>
+                <h1>Modern Day Slavery&nbsp;App</h1>
+            </header>
+            <nav className={styles.menu}>
+                {this.props.languagesLoaded
+                    ? <Button className={styles.button}
+                              onClick={this.props.onBeginQuestions}>Begin Questions (anonymously)</Button>
+                    : <Button className={styles.button} disabled={true}>Loading Data...</Button>}
+                <Button className={styles.button}
+                        onClick={() => this.showLoginScreen()}>Login</Button>
+                <Button className={styles.button}>Help</Button>
+            </nav>
+
+            {this.state.showLogin
+                ? <LoginScreen/>
+                : undefined}
+        </div>;
+    }
 }
